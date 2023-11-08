9bce23cf986aee47dafd73d043fc28f07b289d31
Paco <117916049+Paco-Slavov@users.noreply.github.com>
2023-11-09 00:17:17 +0200

    Delete BookShop.Data directory

--- BookShop.Data/Configuration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BookShop.Data
-{
-    internal class Configuration
-    {
-        internal static string ConnectionString
-            => "Server=LAPTOP-7K6KCTR4\\SQLEXPRESS;Database=BookShop;Integrated Security=True;";
-    }
-}
